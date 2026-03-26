03fffbad970512f7029f3aecdf5677a5f8431f66
leticiaSejanoski <131303547+leticiaSejanoski@users.noreply.github.com>
2026-03-26 19:54:12 -0300

    Class Produto criada

--- aula04/Program.cs
+++ aula04/Program.cs
@@ -1,6 +1,7 @@
 using System;
 
-public class Carro{
+public class Carro
+{
     public string fabricante;
     public string modelo;
     public string cor;
@@ -8,49 +9,114 @@ public class Carro{
     public int ano;
     public bool estado;
 
-    public void exibir(){
-         Console.WriteLine($"Fabricante: {fabricante}\nModelo: {modelo}\nCor: {cor}\nAro: {aro}\nAno: {ano}");
+    public void Exibir()
+    {
+        Console.WriteLine($"Fabricante: {fabricante}\nModelo: {modelo}\nCor: {cor}\nAro: {aro}\nAno: {ano}");
     }
 
-    public void ligar(){
-         Console.WriteLine("Ligou");
+    public void Ligar()
+    {
+        if (estado) Console.WriteLine("O carro já está ligado!");
+        else Console.WriteLine("Ligou!");
     }
 
-      public void desligar(){
-         Console.WriteLine("Desligou");
+    public void Desligar()
+    {
+        if (!estado) Console.WriteLine("O carro já está desligado!");
+        else Console.WriteLine("Desligou!");
     }
 }
 
-public class Livro{
+public class Livro
+{
     public string titulo;
     public int ano;
     public bool disponivel;
 
-    public void exibir(){
-         Console.WriteLine($"Título: {titulo}\nAno: {ano}\nDisponível: {disponivel}");
+    public void Exibir()
+    {
+        Console.WriteLine($"Título: {titulo}\nAno: {ano}\nDisponível: {disponivel}");
     }
 
-     public void emprestar(){
-        if(disponivel){
+    public void Emprestar()
+    {
+        if (disponivel)
+        {
             disponivel = false;
-         Console.WriteLine($"Livro '{titulo}' emprestado com sucesso!");
-        }else{
+            Console.WriteLine($"Livro '{titulo}' emprestado com sucesso!");
+        }
+        else
+        {
             Console.WriteLine($"Livro '{titulo}' não está disponível!");
         }
     }
 
-     public void devolver(){
-        if(!disponivel){
+    public void Devolver()
+    {
+        if (!disponivel)
+        {
             disponivel = true;
-         Console.WriteLine($"Livro '{titulo}' devolvido com sucesso!");
-        }else{
-             Console.WriteLine($"Livro '{titulo}' não é possível pois já está devolvido!");
+            Console.WriteLine($"Livro '{titulo}' devolvido com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine($"Livro '{titulo}' não é possível pois já está devolvido!");
         }
     }
 }
 
-public class Program{
-    static void Main(){
+
+public class Produto
+{
+    public string nome;
+    public double preco;
+    public int quantidade;
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Produto: {nome}\nPreco: {preco:F2}\nEstoque: {quantidade}");
+    }
+
+    public void AdicionarEstoque()
+    {
+        Console.Write("Adicionar ao estoque: ");
+        int adicional = int.Parse(Console.ReadLine());
+        quantidade += adicional;
+    }
+
+    public void RemoverEstoque()
+    {
+        Console.Write("Remover do estoque: ");
+        int removido = int.Parse(Console.ReadLine());
+        if (removido <= quantidade)
+        {
+            quantidade -= removido;
+        }
+        else
+        {
+            Console.WriteLine("Estoque insuficiente!");
+        }
+    }
+
+    public void CalcularValorTotal()
+    {
+        double total = quantidade * preco;
+        Console.WriteLine($"Valor total do estoque: R${total:F2}");
+    }
+
+    public void AplicarDesconto(int percentual)
+    {
+        double desconto = percentual / 100.0;
+        preco -= preco * desconto;
+        Console.WriteLine($"Novo preço com desconto de {percentual}%: R${preco:F2}");
+
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
 
         Carro carro1 = new Carro();
         carro1.fabricante = "Toyota";
@@ -60,13 +126,9 @@ public class Program{
         carro1.ano = 2022;
         carro1.estado = false;
 
-        carro1.exibir();
+        carro1.Exibir();
+        carro1.Desligar();
 
-        if(carro1.estado){
-           carro1.desligar();
-        }else{
-            carro1.ligar();
-        }
 
         Console.WriteLine("-------------------------");
 
@@ -78,11 +140,23 @@ public class Program{
 
         Livro[] biblioteca = new Livro[3];
         biblioteca[0] = l1;
-        biblioteca[0].exibir();
-        l1.emprestar();
+        biblioteca[0].Exibir();
+        l1.Emprestar();
+
+        Console.WriteLine("-------------------------");
+
+        Produto p1 = new Produto();
+        p1.nome = "Lápis";
+        p1.preco = 1.99;
+        p1.quantidade = 200;
+
+        p1.ExibirDados();
+        p1.AdicionarEstoque();
+        p1.RemoverEstoque();
+
+        p1.CalcularValorTotal();
+        p1.AplicarDesconto(50);
     }
 
-    
 
-    
 }
